b798f2ce104929d99bbcb2aa7f2b6b565420ac75
Chrisando Ryan <chrisando.siahaan@gmail.com>
2024-03-03 16:02:47 +0700

    Create binus_enrichment_auto_approve_assignments.js

---
+++ binus_enrichment_auto_approve_assignments.js
@@ -0,0 +1,25 @@
+const timer = ms => new Promise(res => setTimeout(res, ms))
+window.alert = function() {
+    console.log.apply(console, arguments);
+};
+
+async function approve_all_assignments() { 
+    // Iterate through students logbooks
+    var students = $('.showStudentAssignmentBtn')
+    for (var i = 0; i < students.length; i++) {
+        let stud = students[i];
+        stud.click();
+        await timer(5000); 
+
+        // Iterate through logbook months
+        let approve_buttons = $('button.button.button-primary');
+        for (var j = 0; j < approve_buttons.length; j++) {
+            let btn = approve_buttons[j];
+            btn.click();
+
+            await timer(5000); 
+        }
+    }
+}
+
+approve_all_assignments();
