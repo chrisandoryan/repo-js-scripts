0cf352e1229b65105a97cf0f0aa13280f82991dd
Chrisando Ryan <chrisando.siahaan@gmail.com>
2024-09-04 07:26:16 +0700

    Update binus_enrichment_auto_approve_assignments.js

--- binus_enrichment_auto_approve_assignments.js
+++ binus_enrichment_auto_approve_assignments.js
@@ -5,13 +5,22 @@ window.alert = function() {
 
 function is_still_loading(refresh_rate=1000) {
     const interval_id = setInterval(() => {
-      if (!document.getElementById('bubble-loading')) {
-          clearInterval(interval_id);
-          console.log("Loading is gone...")
-          return true
-      } else {
-          console.log("Still loading...")
-      }
+        const elements = document.querySelectorAll('#popUpLoader');
+        let foundVisibleElement = false;
+
+        elements.forEach(element => {
+            if (window.getComputedStyle(element).display !== 'none') {
+                foundVisibleElement = true;
+            }
+        });
+
+        if (!foundVisibleElement) {
+            clearInterval(interval_id);
+            console.log("Loading is gone...")
+            return true
+        } else {
+            console.log("Still loading...")
+        }
     }, refresh_rate);
 }
 
@@ -21,8 +30,8 @@ async function approve_all_assignments() {
     for (var i = 0; i < students.length; i++) {
         let stud = students[i];
         stud.click();
-        is_still_loading(500);
-        await timer(1000); 
+        is_still_loading(1000);
+        await timer(3000); 
 
         // Iterate through logbook months
         let approve_buttons = $('button.button.button-primary');
@@ -30,8 +39,8 @@ async function approve_all_assignments() {
             let btn = approve_buttons[j];
             btn.click();
             console.log(`Approving ${j} of ${approve_buttons.length} for student ${i}`)
-            is_still_loading(500);
-            await timer(1000);
+            is_still_loading(1000);
+            await timer(3000);
             console.log(`Approved.`)
         }
     }
